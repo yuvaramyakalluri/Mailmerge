b783c80f84848795424a7dcc61bfb6bfbf6c1f10
yuvaramyakalluri <165016008+yuvaramyakalluri@users.noreply.github.com>
2026-09-22 15:23:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ Usage:
     python main.py
     python main.py --help
 """
+import http.server
+import os
+import socketserver
+
 from certificate_agent.main import main
 
 if __name__ == "__main__":
     main()
+
+    # --- KEEP FREE RENDER WEB SERVICE ONLINE ---
+    PORT = int(os.environ.get("PORT", 10000))
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving background task status on port {PORT}")
+        httpd.serve_forever()
